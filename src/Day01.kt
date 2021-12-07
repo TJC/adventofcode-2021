313c8d7c6f365338d4ecb177ed3cdcb70ff01094
Toby Corkindale <tjc@wintrmute.net>
2021-12-07 15:00:22 +1100

    solve day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,28 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        val offset1 = input.subList(1, input.size)
+        val pairs = input.zip(offset1)
+        return pairs.filter { (i, j) -> j > i }.size
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        val offset1 = input.subList(1, input.size)
+        val offset2 = input.subList(2, input.size)
+        val trips = input.zip(offset1).zip(offset2)
+        val sums = trips.map {
+            val i = it.first.first
+            val j = it.first.second
+            val k = it.second
+            i+j+k
+        }
+        val combined = sums.zip(sums.subList(1, sums.size))
+        return combined.filter { (a,b) -> b > a }.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("puzzle-1-input.txt")
+    val intList = testInput.map { it.toInt() }
+    println(part1(intList))
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(part2(intList))
+
 }
